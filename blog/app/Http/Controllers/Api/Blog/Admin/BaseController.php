3f5e60b559bfac3c35cc1d990c8214c6dd27d208
Mabertt <pashadoroshuk21999@gmail.com>
2026-06-16 09:11:03 +0000

    lab4

---
+++ blog/app/Http/Controllers/Api/Blog/Admin/BaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog\Admin;
+
+// Чітко вказуємо шлях до нашого минулого базового контролера
+use App\Http\Controllers\Api\Blog\BaseController as BlogBaseController;
+
+abstract class BaseController extends BlogBaseController
+{
+    // Тут порожньо, як і просить методичка
+}
--- blog/routes/api.php
+++ blog/routes/api.php
@@ -7,3 +7,12 @@ use App\Http\Controllers\Api\Blog\PostController;
 Route::group(['prefix' => 'blog'], function () {
     Route::apiResource('posts', PostController::class)->names('blog.posts');
 });
+
+use App\Http\Controllers\Api\Blog\Admin\CategoryController as AdminCategoryController;
+
+// Група маршрутів для адмін-панелі блогу
+Route::prefix('admin/blog')->group(function () {
+    Route::apiResource('categories', AdminCategoryController::class)
+        ->only(['index', 'store', 'update'])
+        ->names('blog.admin.categories');
+});
